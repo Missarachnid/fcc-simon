613d42b3c0da50443552fb1093c6a2e3189bf808
M. Kepler <mmkepler@yahoo.com>
2016-11-06 06:37:49 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,15 +12,12 @@ $(document).ready(function() {
     //variables
     score: $("#score"),
     strict: false,
-    playerTurn: false,
     powerOn: false,
-    gameStart: false,
     gameMoves: [],
     gameSequence: [],
     screenStart: "--",
     level: 1,
     moveCount: 0,
-
     sounds: {
       sound1: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
       sound2: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
@@ -28,18 +25,16 @@ $(document).ready(function() {
       sound4: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
       winSound: new Audio("https://crossorigin.me/http://www.mmkepler.com/other/321918__bboyjoe-15__you-win.mp3"),
       loseSound: new Audio("https://crossorigin.me/http://www.mmkepler.com/other/234257__makkuzu__gameover.mp3"),
-      wrongMove: new Audio("https://crossorigin.me/http://www.mmkepler.com/other/171494__fins__error.wav")
+      wrongMove: new Audio("https://crossorigin.me/http://www.mmkepler.com/other/Electronic_Chime-KevanGC-495939803.mp3")
     },
-
     func: {
       arrayCreate: function() {
-        var count = 20;
+        var count = 4;
         while (count > 0) {
           var num = Math.floor((Math.random() * 4) + 1);
           game.gameMoves.push(num);
           count--;
         }
-        console.log(game.gameMoves);
       },
       yellow: function() {
         game.sounds.sound1.play();
@@ -76,12 +71,10 @@ $(document).ready(function() {
       gameOver: function() {
         game.sounds.loseSound.play();
         game.score.html(game.screenStart);
-        setTimeout(game.func.reset, 3000)();
       },
       winGame: function() {
         game.sounds.winSound.play();
         game.score.html(game.screenStart);
-        setTimeout(game.func.reset, 3000)();
       },
       reset: function() {
         game.level = 1;
@@ -89,90 +82,84 @@ $(document).ready(function() {
         game.gameMoves = [];
         game.gameSequence = [];
         game.playerTurn = false;
+        game.func.disableColors();
         game.func.arrayCreate();
       },
       checkMatch: function(item) {
-        //console.log(item + " item");
-        //console.log(game.gameSequence[game.moveCount] + " gameSequence at move count");
-        console.log(game.moveCount + " move count at input");
         if (parseInt(item) === parseInt(game.gameSequence[game.moveCount])) {
           game.moveCount++;
-          console.log(game.moveCount + " Move count at output");
-          console.log("is equal");
           if (game.level === game.gameMoves.length && game.moveCount === game.gameMoves.length) {
             game.func.winGame();
           } else if (game.moveCount === game.gameSequence.length && game.level < game.gameMoves.length) {
-            game.playerTurn = false;
+            console.log("for next turn " + Boolean(game.playerTurn));
             game.func.nextTurn();
-            game.moveCount = 0;
           }
-        } else if (parseInt(item) !== parseInt(game.gameSequence[game.moveCount])) {
-          console.log("is not equal");
+       } else if (parseInt(item) !== parseInt(game.gameSequence[game.moveCount])) {
+          
           if (game.strict === true) {
             game.func.gameOver();
           } else {
             game.sounds.wrongMove.play();
-            game.playerTurn = false;
-            game.func.animator();
+           game.func.wrongTurn();
           }
         }
       },
       nextTurn: function() {
         game.level++;
+        game.moveCount = 0;
         game.score.html(game.level);
-        game.func.animator();
+        game.func.disableColors();
+        setTimeout(game.func.animator, 1500);
+      },
+      wrongTurn: function() {
+        game.moveCount = 0;
+        game.func.disableColors();
+        setTimeout(game.func.animator, 1500);
+      },
+
+      myDelay: function(n) {
+        var temp = game.gameSequence[n];
+        var speed;
+
+        if (game.level < 5) {
+          speed = 1300;
+        } else if (game.level >= 5 && game.level < 9) {
+          speed = 1100;
+        } else if (game.level >= 9 && game.level < 13) {
+          speed = 900;
+        } else {
+          speed = 750;
+        }
+        switch (temp) {
+          case 1:
+            window.setTimeout(game.func.yellow, speed * n);
+            break;
+          case 2:
+            window.setTimeout(game.func.green, speed * n);
+            break;
+          case 3:
+            window.setTimeout(game.func.red, speed * n);
+            break;
+          case 4:
+            window.setTimeout(game.func.blue, speed * n);
+            break;
+        }
       },
       animator: function() {
         setTimeout(function() {
-          game.playerTurn = false;
           game.gameSequence = game.gameMoves.slice(0, game.level);
           console.log(game.gameSequence);
           for (var i = 0; i <= game.gameSequence.length - 1; i++) {
-            if (game.powerOn === false) {
-              return false;
-            }
-            (function myDelay(n) {
-              var temp = game.gameSequence[n];
-              var speed;
-              if (game.level < 5) {
-                speed = 1300;
-              } else if (game.level >= 5 && game.level < 9) {
-                speed = 1100;
-              } else if (game.level >= 9 && game.level < 13) {
-                speed = 900;
-              } else {
-                speed = 750;
-              }
-
-              switch (temp) {
-                case 1:
-                  window.setTimeout(game.func.yellow, speed * n);
-                  break;
-                case 2:
-                  window.setTimeout(game.func.green, speed * n);
-                  break;
-                case 3:
-                  window.setTimeout(game.func.red, speed * n);
-                  break;
-                case 4:
-                  window.setTimeout(game.func.blue, speed * n);
-                  break;
-              }
-              
-            if(n === game.gameSequence.length - 1){
+            game.func.myDelay(i);
+            if (i === game.gameSequence.length - 1) {
               game.playerTurn = true;
+              game.func.enableColors();
+            }else{
+              game.func.disableColors();
             }
-            })(i);
-
-            
           }
-
-          
         }, 1500);
-
-        
       },
-      
       disableToggle: function() {
         if ($(".choice").prop("disabled")) {
           $(".choice").prop("disabled", false);
@@ -180,63 +167,29 @@ $(document).ready(function() {
           $(".choice").prop("disabled", true);
         }
       },
-      squareToggle: function(){
-        if($(".square").prop("disabled")){
-          $(".square").prop("disabled", false);
-        }else {
-          $(".square").prop("disabled", true);
-        }
+      disableColors: function() {
+        $("#green, #yellow, #red, #blue").prop("disabled", true);
+      },
+      enableColors: function() {
+        $("#green, #yellow, #red, #blue").prop("disabled", false);
       }
     },
     buttons: {
       yellowClick: $("#yellow").click(function() {
-        //if(game.playerTurn === true){
-        //game.func.yellow();
-        //game.func.checkMatch(1);
-        //} else {
-        //return false;
-        //}
-
         game.func.yellow();
         game.func.checkMatch(1);
       }),
       greenClick: $("#green").click(function() {
-
-        //if(game.playerTurn === true){
-        //game.func.green();
-        //game.func.checkMatch(2);
-        //} else {
-        // return false;
-        //}
-
         game.func.green();
         game.func.checkMatch(2);
-
       }),
       redClick: $("#red").click(function() {
-
-        //if(game.playerTurn === true){
-        // game.func.red();
-        //game.func.checkMatch(3);
-        //} else {
-        //return false;
-        //}
-
         game.func.red();
         game.func.checkMatch(3);
       }),
       blueClick: $("#blue").click(function() {
-
-        //if(game.playerTurn === true){
-        //game.func.blue();
-        //game.func.checkMatch(4);
-        //} else {
-        //return false;
-        //}
-
         game.func.blue();
         game.func.checkMatch(4);
-
       }),
       startClick: $("#start").click(function() {
         game.func.reset();
@@ -261,6 +214,7 @@ $(document).ready(function() {
         } else {
           game.powerOn = false;
           game.func.disableToggle();
+          game.func.disableColors();
           $(".light").css("background-color", "black");
           game.func.reset();
           game.score.empty();
@@ -269,14 +223,11 @@ $(document).ready(function() {
     }
   };
 
-  if(game.playerTurn){
-    $(".square").attr("disabled", false);
-  } else if(game.playerTurn === false) {
-    $(".square").attr("disabled", true);
-  }
-
 });
 
 //make disable line up with animation time
-//Is it possible to put an if/else in at the end of the for to set playerTurn to true?
-//add timeout for player turn
+//add timeout for player turn so turns won't take so long.
+//problems with audio from dropbox - find solution
+
+//from current state, buttons are disabled at start, but not during animation.
+//the function to disable them was moved inside. When start button is pushed
